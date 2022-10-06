034b5dee6d574f3ecb3b49608e197e052d5f2b70
ryan <rmurney@gmail.com>
2022-10-06 15:57:40 -0300

    baby steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+// hooks
+import { useEffect, useState } from "react";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sideNavBar">
+        <div className="sideNavHeader">
+          <FontAwesomeIcon icon={faBars}/>
+          <h2>SimpleSport</h2>
+        </div>
+        <div className="sideNavBody"></div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
