7d7750503a77d3db8cc7048013e8d2d6b05efba8
ryan <rmurney@gmail.com>
2022-10-19 14:35:23 -0300

    navbar open close

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = ({ setScreen }) => {
-  // const []
+  const [navState, setNavState] = useState(Boolean);
+
+  useEffect(() => {
+    setNavState(true);
+  }, []);
+
+  const navBarOpenClose = () => {
+    setNavState(!navState);
+    
+    if (navState) {
+      console.log(navState, 'was true');  
+
+      
+    } else {
+      console.log(navState, 'was false');
+
+    }
+  };
 
   return (
-      <div className="sideNavBar">
+      <div className={ navState ? "sideNavBar" : "sideNavBar sideBarHidden"}>
       <div className="sideNavHeader">
-        <FontAwesomeIcon icon={faBars}/>
+        <FontAwesomeIcon icon={faBars} onClick={() => navBarOpenClose()} />
         <h2 onClick={() => setScreen('Dashboard')}>SimpleSport</h2>
       </div>
-      <div className="sideNavBody">
+      <div className={ navState ? "sideNavBody" : "hidden"}>
           <div onClick={() => setScreen('Schedule')} className="sideNavItem"><FontAwesomeIcon icon={faChevronRight}/><h3>Schedule</h3></div>
           <div onClick={() => setScreen('Roster')} className="sideNavItem"><FontAwesomeIcon icon={faChevronRight}/><h3>Roster</h3></div>
           <div onClick={() => setScreen('Messenger')} className="sideNavItem"><FontAwesomeIcon icon={faChevronRight}/><h3>Messenger</h3></div>
